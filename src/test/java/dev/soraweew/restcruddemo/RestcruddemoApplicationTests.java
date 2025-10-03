579869b9a49f42c67a525a78db8c172821cf9a46
soraweew <sorawee.wata@gmail.com>
2025-10-03 15:29:58 +0700

    Initial commit

---
+++ src/test/java/dev/soraweew/restcruddemo/RestcruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.soraweew.restcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcruddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
