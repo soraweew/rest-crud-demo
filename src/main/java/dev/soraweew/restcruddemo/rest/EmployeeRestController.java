de1662cd78ced5b50e37c7f0a382289de1885ca6
soraweew <sorawee.wata@gmail.com>
2025-10-04 16:51:22 +0700

    Add get by id, add, update in controller

--- src/main/java/dev/soraweew/restcruddemo/rest/EmployeeRestController.java
+++ src/main/java/dev/soraweew/restcruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package dev.soraweew.restcruddemo.rest;
 import dev.soraweew.restcruddemo.entity.Employee;
 import dev.soraweew.restcruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,36 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee is not found - " + id);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        // Force a save of new item instead of update for the behavior of Entity Manager's merge
+        employee.setId(0);
+
+        Employee dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        Employee dbEmployee = employeeService.save(employee);
+
+        return employee;
     }
 }
