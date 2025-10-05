3707aa94a369abe781530806b96d74bc037e93dd
soraweew <sorawee.wata@gmail.com>
2025-10-05 15:42:33 +0700

    Add patch mapping to controller

--- src/main/java/dev/soraweew/restcruddemo/rest/EmployeeRestController.java
+++ src/main/java/dev/soraweew/restcruddemo/rest/EmployeeRestController.java
@@ -1,11 +1,14 @@
 package dev.soraweew.restcruddemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.soraweew.restcruddemo.entity.Employee;
 import dev.soraweew.restcruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -13,9 +16,12 @@ public class EmployeeRestController {
 
     private EmployeeService employeeService;
 
+    private ObjectMapper objectMapper;
+
     @Autowired
-    public EmployeeRestController(EmployeeService employeeService) {
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper) {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/employees")
@@ -52,5 +58,38 @@ public class EmployeeRestController {
         Employee dbEmployee = employeeService.save(employee);
 
         return employee;
+    }
+
+    @PatchMapping("/employees/{id}")
+    public Employee patchEmployee(@PathVariable int id,
+                                  @RequestBody Map<String, Object> patchPayload) {
+
+        Employee tempEmployee = employeeService.findById(id);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + id);
+        }
+
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + id);
+        }
+
+        Employee patchedEmployee = apply(patchPayload, tempEmployee);
+
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+        // Convert employee object to a JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+        // Convert the patchPayload map to JSON object node
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
     }
 }
