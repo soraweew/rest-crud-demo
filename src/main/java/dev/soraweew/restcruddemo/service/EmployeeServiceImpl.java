a8d0be6bd1ac0d4afd80617de56833d923548223
soraweew <sorawee.wata@gmail.com>
2025-10-05 19:17:59 +0700

    Replace dao with JpaRepository

--- src/main/java/dev/soraweew/restcruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/dev/soraweew/restcruddemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,40 @@
 package dev.soraweew.restcruddemo.service;
 
-import dev.soraweew.restcruddemo.dao.EmployeeDAO;
+import dev.soraweew.restcruddemo.dao.EmployeeRepository;
 import dev.soraweew.restcruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Did not find employee id - " + id));
     }
 
-    @Transactional
     @Override
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
-    @Transactional
     @Override
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
